79f102dfae9f66bc0b34e02d8ccddec5962aa7b4
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2022-09-01 09:30:44 +0300

    Configure data source for Postgres, and initialize schema.

---
+++ src/main/java/com/example/todo/configs/PostgresConfig.java
@@ -0,0 +1,47 @@
+package com.example.todo.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+@Configuration
+public class PostgresConfig {
+    @Bean
+    public DataSource postgresDataSource(
+            @Value("${postgres.credentials.folder}") String secretsFolder,
+            @Value("${postgres.credentials.passwordFile}") String passwordFile,
+            @Value("${postgres.credentials.usernameFile}") String usernameFile,
+            @Value("${postgres.credentials.urlFile}") String urlFile
+    ) throws IOException {
+        Path folder = Path.of(secretsFolder);
+        String password = read(folder, passwordFile);
+        String username = read(folder, usernameFile);
+        String url = read(folder, urlFile);
+        log.info("Secrets: " + password + ", " + username + ", " + url);
+        return dataSource(url, username, password);
+    }
+
+    private String read(Path folder, String fileName) throws IOException {
+        Path file = folder.resolve(fileName);
+        return Files.readString(file);
+    }
+
+    private DataSource dataSource(String url, String username, String password) {
+        return DataSourceBuilder
+                .create()
+                .driverClassName("org.postgresql.Driver")
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+}
